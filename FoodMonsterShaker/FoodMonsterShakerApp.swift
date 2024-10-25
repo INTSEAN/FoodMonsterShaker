79a2125f12559fa679fc08505fecc9b280e5eb07
INTSEAN <119046561+INTSEAN@users.noreply.github.com>
2024-10-24 20:36:46 -0400

    Initial Commit

---
+++ FoodMonsterShaker/FoodMonsterShakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMonsterShakerApp.swift
+//  FoodMonsterShaker
+//
+//  Created by SEAN DONOVAN on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodMonsterShakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodMonsterShakerTests/FoodMonsterShakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMonsterShakerTests.swift
+//  FoodMonsterShakerTests
+//
+//  Created by SEAN DONOVAN on 10/24/24.
+//
+
+import Testing
+@testable import FoodMonsterShaker
+
+struct FoodMonsterShakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
